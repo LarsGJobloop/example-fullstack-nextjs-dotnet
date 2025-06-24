d2e2ae4829c9408fc64d005564a15da1ea92dac5
zabronax <zabronax@gmail.com>
2025-06-24 15:20:39 +0200

    setup wildcard cors policy

--- apps/backend/Program.cs
+++ apps/backend/Program.cs
@@ -1,5 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy("AllowAll", policy =>
+  {
+    policy
+      .AllowAnyOrigin()
+      .AllowAnyHeader()
+      .AllowAnyMethod();
+  });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/", () => "Hello World!");
 
