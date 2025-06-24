ab5617cabc55f93345e76ef87b7dd4b24aa2e1f0
zabronax <zabronax@gmail.com>
2025-06-24 15:20:53 +0200

    move to server side rendering

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,16 +1,17 @@
-"use client"
+// "use client"
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [data, setData] = useState<string | null>(null)
-
-
-  useEffect(() => {
-    fetch("http://localhost:5176")
-      .then(response => response.json())
-      .then(payload => setData(payload))
-  }, [])
+export default async function Home() {
+  const response = await fetch("http://localhost:5176")
+  const data = await response.text()
+  
+  // const [data, setData] = useState<string | null>(null)
+  // useEffect(() => {
+  //   fetch("http://localhost:5176")
+  //     .then(response => response.text())
+  //     .then(payload => setData(payload))
+  // }, [])
 
   return (
     <div>
